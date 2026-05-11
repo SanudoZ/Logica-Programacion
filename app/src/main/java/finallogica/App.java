cfd8daa4188ef4f0ce0bb4bbd3b508c37eb591ca
SanudoZ <sanudo.sebastian2008@gmail.com>
2026-05-11 12:27:44 -0500

    Primer switch y primer ciclo

--- app/src/main/java/finallogica/App.java
+++ app/src/main/java/finallogica/App.java
@@ -11,15 +11,71 @@ public class App {
 
             // Variables
 
+            int Usuario = 0;
+            int producto = 0;
+
             Scanner sc = new Scanner(System.in);
 
             // Datos de entrada
 
+            System.out.println("¿Que tipo de usuario es?' 1. Cliente | 2. Administrador: ");
+            Usuario = sc.nextInt();
+            sc.nextLine();
+
             // Proceso
+
+            if (Usuario == 1) {
+
+                while (true) {
+
+                    System.out.println("¿Que desea comprar? 1. Camisa | 2. Pantalon | 3. Zapatos | 4. Salir: ");
+                    producto = sc.nextInt();
+                    sc.nextLine();
+
+                }
+
+            }
 
             // Datos de salida
 
         } catch (Exception e) {
+
+        }
+
+    }
+
+    // Funciones
+
+    public static int precios(int producto) {
+
+        try {
+            int precio = 0;
+
+            switch (producto) {
+
+                case 1:
+                    precio = 1000;
+                    break;
+
+                case 2:
+                    precio = 2000;
+                    break;
+
+                case 3:
+                    precio = 3000;
+                    break;
+
+                default:
+                    System.out.println("Producto no encontrado");
+                    break;
+
+            }
+
+            return precio;
+
+        } catch (Exception e) {
+
+            return -1;
 
         }
 
