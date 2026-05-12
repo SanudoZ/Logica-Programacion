08c4be3c0f1835e4b51ff1e8f42e8fa65fb06fba
SanudoZ <sanudo.sebastian2008@gmail.com>
2026-05-11 19:07:16 -0500

    Precios, ciclo anidado y primer funcion

--- app/src/main/java/finallogica/App.java
+++ app/src/main/java/finallogica/App.java
@@ -13,24 +13,52 @@ public class App {
 
             int Usuario = 0;
             int producto = 0;
+            int cantidad = 0;
 
             Scanner sc = new Scanner(System.in);
 
             // Datos de entrada
 
-            System.out.println("¿Que tipo de usuario es?' 1. Cliente | 2. Administrador: ");
-            Usuario = sc.nextInt();
-            sc.nextLine();
+            while (true) {
 
-            // Proceso
+                System.out.println("¿Que tipo de usuario es?' 1. Cliente | 2. Administrador | 3. Salir: ");
+                Usuario = sc.nextInt();
+                sc.nextLine();
 
-            if (Usuario == 1) {
+                if (Usuario == 3) {
+                    System.out.println("Vuelva pronto");
+                    break;
+                }
 
-                while (true) {
+                // Proceso
 
-                    System.out.println("¿Que desea comprar? 1. Camisa | 2. Pantalon | 3. Zapatos | 4. Salir: ");
-                    producto = sc.nextInt();
-                    sc.nextLine();
+                if (Usuario == 1) {
+
+                    while (true) {
+
+                        System.out.println("¿Que desea comprar? 1. Camisa | 2. Pantalon | 3. Zapatos | 4. Salir: ");
+                        producto = sc.nextInt();
+                        sc.nextLine();
+
+                        if (producto == 4) {
+                            System.out.println("Gracias por elegirnos");
+
+                            break;
+                        }
+
+                        else if (producto >= 1 && producto <= 3) {
+
+                            System.out.println(" ¿Cuantas desea comprar?");
+                            cantidad = sc.nextInt();
+                            sc.nextLine();
+
+                        }
+
+                        else {
+                            System.out.println("Producto no encontrado");
+                        }
+
+                    }
 
                 }
 
@@ -46,23 +74,24 @@ public class App {
 
     // Funciones
 
-    public static int precios(int producto) {
+    public static int precios(int producto, int cantidad) {
 
         try {
+
             int precio = 0;
 
             switch (producto) {
 
                 case 1:
-                    precio = 1000;
+                    precio = 1000 * cantidad;
                     break;
 
                 case 2:
-                    precio = 2000;
+                    precio = 2000 * cantidad;
                     break;
 
                 case 3:
-                    precio = 3000;
+                    precio = 3000 * cantidad   ;
                     break;
 
                 default:
